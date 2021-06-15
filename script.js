399b545608f6195e75c0aa1056cc5596766d24ee
Keely Bailey <keelybailey3@gmail.com>
2021-06-15 14:02:20 -0500

    Add fetch to pull in weather api - success

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
+const BASE_URL = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=c9e96d3d81e462961b72fff443b910ae'
+
 const cityFormEl = document.getElementById('searchCity')
 const currentConditionsEl = document.getElementById('current-conditions')
 const forecastEl = document.getElementById('forecast')
 
 
+/*
 const formSubmitHandler = function (event) => {
+
   event.preventDefault();
 
   const searchCity = document.getElementById('searchCity').trim()
@@ -15,3 +19,15 @@ const formSubmitHandler = function (event) => {
   }
 }
 */
+
+//function getSearchCityWeather(city) {
+
+  fetch(BASE_URL)
+    .then(function (res) {
+      return res.json()
+    })
+    .then(function (data){
+      console.log(data)
+    })
+
+//api key c9e96d3d81e462961b72fff443b910ae
