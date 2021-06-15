7ec74836f006915ecdd0bb5f33637fd5eb17bf6d
Keely Bailey <keelybailey3@gmail.com>
2021-06-15 13:23:07 -0500

    Add DOM elements and started form submit handler

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
+const cityFormEl = document.getElementById('searchCity')
+const currentConditionsEl = document.getElementById('current-conditions')
+const forecastEl = document.getElementById('forecast')
 
 
 const formSubmitHandler = function (event) => {
   event.preventDefault();
 
-  const searchCity =
+  const searchCity = document.getElementById('searchCity').trim()
+
+  if (searchCity) {
+    getSearchCityWeather(searchCity)
+
+
+  }
 }
+*/
