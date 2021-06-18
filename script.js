3959201ed1d909019b6d26c03f90417d58b9e6ed
Keely Bailey <keelybailey3@gmail.com>
2021-06-18 14:29:33 -0500

    Try out async again

--- script.js
+++ script.js
@@ -1,64 +1,36 @@
-const BASE_URL = 'http://api.openweathermap.org/data/2.5/forecast?appid=c9e96d3d81e462961b72fff443b910ae'
+const API_URL = 'http://api.openweathermap.org/data/2.5/forecast?q=chicago&appid=c9e96d3d81e462961b72fff443b910ae'
 
 const apiKey = "c9e96d3d81e462961b72fff443b910ae"
 
+const main = document.getElementById('main')
 const cityFormEl = document.getElementById('searchCity')
-const currentConditionsEl = document.getElementById('current-conditions')
 const forecastEl = document.getElementById('forecast')
 const searchCity = document.getElementById('searchCity')
 const TusconBtn = document.getElementById('Tuscon').addEventListener('click', Tuscon)
 
-/*
-const formSubmitHandler = (event) => {
+getWeather(API_URL)
 
-  event.preventDefault();
-
-  if (searchCity) {
-    getSearchCityWeather(searchCity)
-
-
-  }
-}
-*/
-//importing city, convert to json
-
-//JSON parse map function to pull in the city object/details
-
-//then pull the keys into the html from there
-
-/*
-const getCity = async(city)= {
-  const new_url = 'http://api.openweathermap.org/data/2.5/forecast'
-  const query = `?apikey=${apiKey}&q=${city}`
-
-  const response = await fetch(url+query)
-  const data = await response.json()
-  return data[0]
+async function getWeather(url) {
+  const res = await fetch(url)
+  const data = await res.json()
+  showCurrentConditions(data.results)
 }
 
-console.log(getCity)*/
+function showCurrentConditions(weather) {
+  main.innerHTML = ''
 
+  weather.forEach((conditions) => {
+    const {icon, description, temp, wind, humidity} = conditions
 
-cityFormEl.addEventListener('submit', e =>{
-  e.preventDefault()
-  const inputVal = input.value.trim()
-  searchCity = inputVal
-})
-console.log(searchCity)
+    const currentConditionsEl = document.createElement('div')
+    currentConditionsEl.classList.add('weather-info')
 
-function getSearchCityWeather(searchCity) {
-  fetch(cityURL)
-    .then(function (res) {
-      return res.json()
-    })
-    .then(function (data){
-    /*  let output = '<h2>City</h2>'
-            output += `
-            <div>
-            <h3>${city.name}</h3>
-            </div>`*/
-            console.log(data)
-          })
-          //document.getElementById('currentConditionsEl').innerHTML = output
-        }
-        getSearchCityWeather()
+    currentConditionsEl.innerHTML =
+    `
+    <span>Temp: ${temp}</span>
+    <span>Wind: ${wind}</span>
+    <span>Humidity: ${humidity}</span>
+    `
+    main.appendChild(currentConditionsEl)
+  })
+}
